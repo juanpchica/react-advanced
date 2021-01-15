8ef563ee266d854a12218135ea946c197c9cba1e
juanpchica <juanpchica@hotmail.com>
2021-01-15 16:25:59 -0500

    Conditionla rendering finished

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
@@ -6,7 +6,32 @@ const ShortCircuit = () => {
   // const firstValue = text || 'hello world';
   // const secondValue = text && 'hello world';
 
-  return <h2>short circuit</h2>;
+  const [text, setText] = useState("");
+  const [isError, setIsError] = useState(false);
+  const firstValue = text || "First value";
+  const secondValue = text && "Second Value";
+
+  return (
+    <>
+      <h2>{text || "First Value validation"}</h2>
+      {!text && <h2>#hello</h2>}
+      {isError && <h4>There is an error...</h4>}
+      {isError ? (
+        <h3>There is an error my friend</h3>
+      ) : (
+        <div>
+          <h3>Without Error</h3>
+        </div>
+      )}
+      <button
+        className="btn"
+        type="button"
+        onClick={() => setIsError(!isError)}
+      >
+        Toogle Text
+      </button>
+    </>
+  );
 };
 
 export default ShortCircuit;
--- src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,36 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
+  const [isShow, setIsShow] = useState(false);
+  return (
+    <React.Fragment>
+      <h2>show/hide</h2>
+      {isShow && <Item />}
+      <button className="btn" type="button" onClick={() => setIsShow(!isShow)}>
+        Show/Hide
+      </button>
+    </React.Fragment>
+  );
+};
+
+const Item = () => {
+  const [size, setSize] = useState(window.innerWidth);
+  const checksize = function () {
+    setSize(window.innerWidth);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", checksize);
+    return () => {
+      window.removeEventListener("resize", checksize);
+    };
+  }, []);
+
+  return (
+    <>
+      <h2>Window Width</h2>
+      <h2>{size} px</h2>
+    </>
+  );
 };
 
 export default ShowHide;
