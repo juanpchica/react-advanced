cbf62a281ddf33ed12fdcedd63f5fbf9c9eb949a
juanpchica <juanpchica@hotmail.com>
2021-01-14 23:14:20 -0500

    Multiple returns finished

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,30 @@
-import React, { useState, useEffect } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+import React, { useState, useEffect } from "react";
+const url = "https://api.github.com/users/QuincyLarson";
+let data = [];
 const MultipleReturns = () => {
-  return <h2>multiple returns</h2>;
+  const [loading, setloading] = useState(true);
+  const getData = async () => {
+    const response = await fetch(url);
+    data = await response.json();
+    setloading(false);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <>
+      <div className="item">
+        <img src={data.avatar_url} alt="" />
+        <h2>{data.name}</h2>
+      </div>
+    </>
+  );
 };
 
 export default MultipleReturns;
