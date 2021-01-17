ada0f4d539261b917d91cf3bf07650b7578a8d3b
juanpchica <juanpchica@hotmail.com>
2021-01-16 21:19:31 -0500

    useReducer example finished

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Setup from "./tutorial/6-useReducer/setup/index";
+import Setup from "./tutorial/6-useReducer/setup";
 function App() {
   return (
     <div className="container">
