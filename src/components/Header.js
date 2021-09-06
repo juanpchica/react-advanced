5489729c17df654526e5a1d6e51a85787fac7c95
Juan Pablo Chica <juanpchica@hotmail.com>
2021-09-06 11:09:21 -0500

    Proptypes and defaultProps tested

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,20 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 
-const Header = () => {
+const Header = (props) => {
+    console.log(props);
     return (
         <div>
             Header
-        </div>
+        </div> 
     )
+}
+Header.defaultProps = {
+    title:"this is the title"
+}
+
+Header.propTypes = {
+    title:PropTypes.string
 }
 
 export default Header
--- src/pages/Task.js
+++ src/pages/Task.js
@@ -1,8 +1,11 @@
+import React from 'react'
+
+import Header from "../components/Header"
 
 const Task = () => {
     return (
         <div>
-          task  
+          <Header title={2}/> 
         </div>
     )
 }
