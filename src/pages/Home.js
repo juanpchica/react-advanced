ae497b50b4447120370f05a88ef75795865d7f30
Juan Pablo Chica <juanpchica@hotmail.com>
2021-09-06 10:27:06 -0500

    Creating Home and task components

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Setup from "../tutorial/12-memo-useMemo-useCallback/setup";
+
+const Home = () => {
+    return (
+        <div>
+            <Setup />
+        </div>
+    )
+}
+
+export default Home
---
+++ src/pages/Task.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Task = () => {
+    return (
+        <div>
+          task  
+        </div>
+    )
+}
+
+export default Task
