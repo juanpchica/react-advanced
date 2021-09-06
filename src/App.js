eb08282da921000015d617ce3f4967d0a3d9e1c1
Juan Pablo Chica <juanpchica@hotmail.com>
2021-09-06 10:34:42 -0500

    Implementing router dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from "react";
-import Setup from "./tutorial/12-memo-useMemo-useCallback/setup";
+
+import { Route,Switch,BrowserRouter as Router } from "react-router-dom";
+import Nav from "./components/Nav";
+import Home from "./pages/Home";
+import Task from "./pages/Task";
+
 function App() {
   return (
     <div className="container">
-      <Setup />
+      <Router>
+        <Nav/>
+        <Switch>
+          <Route path="/" exact component={Home}/>
+          <Route path="/task" component={Task}/>
+        </Switch>
+      </Router>
+      
     </div>
   );
 }
