1d1e2fcaf949a69e54d27b6b9852df977cc33f04
Juan Pablo Chica <juanpchica@hotmail.com>
2021-09-06 10:36:47 -0500

    Installing navbar for navigation

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Nav = () => {
     return (
         <div>
-         nav   
+            <ul>
+                <li><Link to="/" >Home</Link></li>
+                <li><Link to="/task" >Task</Link></li>
+            </ul>
         </div>
     )
 }
