be0a53ed5882423f821d1b6fe337116bcc2d5bf6
Juan Pablo Chica <juanpchica@hotmail.com>
2021-09-08 11:21:14 -0500

    Adding validation class in case reminder is true

--- src/components/Task.js
+++ src/components/Task.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {FaTimes} from 'react-icons/fa';
 const Task = ({task:{text,day,reminder,id},onDelete}) => {
     return (
-        <div className="task">
+        <div className={reminder ? "task task-active":"task"}>
             <h3>{text} <FaTimes style={{color:'red',cursor:'pointer'}} onClick={(e)=>{onDelete(id)}}/></h3>
             <p>{day}</p>
         </div>
