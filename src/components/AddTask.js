924c5b09bbb34d1c9d28b5d62d83030811a62ee9
Juan Pablo Chica <juanpchica@hotmail.com>
2021-09-09 16:01:16 -0500

    Adding state nameTask to control input

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const AddTask = () => {
+    const [nameTask,setNameTask] = useState("");
+
     return (
         <div>
             <form>
-                <input type="text" placeholder="Name a new task.."/>
+                <input type="text" placeholder="Name a new task.." onChange={(e)=>setNameTask(e.target.value)}/>
             </form>
         </div>
     )
